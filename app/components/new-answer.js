79a25dfcec11de22aec09349cf048334bc54a266
browneryan <browneryan87@gmail.com>
2016-04-01 11:29:19 -0700

    Add new-answer component, form and sendAction save2

---
+++ app/components/new-answer.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    save1() {
+      var params = {
+        reply: this.get('replyInput') ? this.get('replyInput'): "",
+        author: this.get('authorInput') ? this.get('authorInput'): "",
+        question: this.get('question')
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('save2', params);
+    }
+  }
+});
