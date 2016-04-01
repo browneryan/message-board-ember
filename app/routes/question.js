bfc9f6b2449227955a199f124b043dcc715f2cde
browneryan <browneryan87@gmail.com>
2016-04-01 11:10:46 -0700

    Add answers display to question.hbs, question.js

--- app/routes/question.js
+++ app/routes/question.js
@@ -3,4 +3,26 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   return this.store.findRecord('question', params.question_id);
   },
+  actions: {
+    save3(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      console.log(params.question.get('title'));
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question');
+    },
+    update(answer, params) {
+      console.log(params);
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          answer.set(key, params[key]);
+        }
+      });
+      answer.save();
+      this.transitionTo('question');
+    }
+  }
 });
