4fedd3447aa089d14c76f816a04559d005a58997
browneryan <browneryan87@gmail.com>
2016-04-01 09:33:48 -0700

    Update form input values, ids

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    save1() {
+      var params = {
+        author: this.get('authorInput') ? this.get('authorInput'): "",
+        inquiry: this.get('inquiryInput') ? this.get('inquiryInput'): "",
+        note: this.get('noteInput') ? this.get('noteInput'): ""
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('save2', params);
+    }
+  }
 });
