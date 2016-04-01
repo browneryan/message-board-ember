06cb856c99756e3ff870e83cba9f17c378b6b942
browneryan <browneryan87@gmail.com>
2016-04-01 11:51:14 -0700

    Add update answer form, grab params

--- app/components/update-answer.js
+++ app/components/update-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateAnswerForm: false,
+  actions: {
+    updateAnswerForm() {
+      this.set('updateAnswerForm', true);
+    },
+    update(answer) {
+      var params = {
+        reply: this.get('replyInput') ? this.get('replyInput'): answer.get('replyInput'),
+        author: this.get('authorInput') ? this.get('authorInput'): author.get('answerInput'),
+      };
+      // debugger;
+      this.set('updateAnswerForm', false);
+      this.sendAction('update', answer, params);
+    }
+  }
 });
